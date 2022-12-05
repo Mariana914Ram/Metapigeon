70125985cb8dadbb8fa84c0f89502e8839e68d0b
Mariana Ramírez Llamas <mariana914ramirez@gmail.com>
2022-12-05 12:05:22 -0600

    Revert "Revert "Notificaciones""
    This reverts commit 60e89a450432bd13ac88423427ac989e7a4d9967.

--- app/src/main/java/com/example/metapigeon/MainActivity.java
+++ app/src/main/java/com/example/metapigeon/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.metapigeon;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationManagerCompat;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -12,6 +13,7 @@ import android.util.Log;
 import com.example.metapigeon.ui.main.DBController;
 import java.util.Timer;
 import java.util.TimerTask;
+import static com.example.metapigeon.SpellActivity.NOTIFICACION_ID;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +23,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String menuFragment = getIntent().getStringExtra("option");
+        if(menuFragment != null){
+            //Eliminar la notificación de barra de estado
+            NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(getApplicationContext());
+            notificationManagerCompat.cancel(NOTIFICACION_ID);
+        }
 
         admin = new DBController(this,"metapigeonDB",null,1);
 
